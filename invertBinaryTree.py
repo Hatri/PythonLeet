2551471a0595f53c1695c8f24bee55d6dd12b026
Hatri <18404171+Hatri@users.noreply.github.com>
2020-07-08 21:14:30 +0200

    Updated.

--- invertBinaryTree.py
+++ invertBinaryTree.py
@@ -14,3 +14,19 @@ Output:
  / \   / \
 9   6 3   1
 '''
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        queue = []
+        queue.append(root)
+        while queue:
+            current = queue.pop(0)
+            current.left, current.right = current.right, current.left
+        if current.left:
+            queue.append(current.left)
+        else:
+            queue.append(current.right)
+        return root
